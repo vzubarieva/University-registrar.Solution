ff19f2b6ebdb3ccc657039864ac8490e6c07f14e
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-07 11:24:14 -0700

    update index and detail route

--- University-registrar/Controllers.cs/StudentsController.cs
+++ University-registrar/Controllers.cs/StudentsController.cs
@@ -1,75 +1,77 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using University-registrar.Models;
-// using System.Collections.Generic;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace University-registrar.Controllers
-// {
-//     public class StudentsController : Controller
-//     {
-//         private readonly UniversityregistrarContext _db;
+namespace UniversityRegistrar.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly UniversityRegistrarContext _db;
 
-//         public StudentsController(UniversityregistrarContext db)
-//         {
-//             _db = db;
-//         }
+        public StudentsController(UniversityRegistrarContext db)
+        {
+            _db = db;
+        }
 
-//         public ActionResult Index()
-//         {
-//             List<Cuisine> model = _db.Cuisines.Include(cuisine => cuisine.Restaurant).ToList();
-//             return View(model);
-//         }
+        public ActionResult Index()
+        {
+            return View(_db.Students.ToList());
+        }
 
-//         public ActionResult Create()
-//         {
-//             ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
-//             return View();
-//         }
+        //         public ActionResult Create()
+        //         {
+        //             ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
+        //             return View();
+        //         }
 
-//         [HttpPost]
-//         public ActionResult Create(Cuisine cuisine)
-//         {
-//             _db.Cuisines.Add(cuisine);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        //         [HttpPost]
+        //         public ActionResult Create(Cuisine cuisine)
+        //         {
+        //             _db.Cuisines.Add(cuisine);
+        //             _db.SaveChanges();
+        //             return RedirectToAction("Index");
+        //         }
 
-//         public ActionResult Details(int id)
-//         {
-//             Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-//             return View(thisCuisine);
-//         }
+        public ActionResult Details(int id)
+        {
+            Student thisStudent = _db.Students
+                .Include(student => student.JoinEntities)
+                .ThenInclude(join => join.Course)
+                .FirstOrDefault(student => student.StudentId == id);
+            return View(thisStudent);
+        }
 
-//         public ActionResult Edit(int id)
-//         {
-//             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-//             ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
-//             return View(thisCuisine);
-//         }
+        //         public ActionResult Edit(int id)
+        //         {
+        //             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+        //             ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
+        //             return View(thisCuisine);
+        //         }
 
-//         [HttpPost]
-//         public ActionResult Edit(Cuisine cuisine)
-//         {
-//             _db.Entry(cuisine).State = EntityState.Modified;
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        //         [HttpPost]
+        //         public ActionResult Edit(Cuisine cuisine)
+        //         {
+        //             _db.Entry(cuisine).State = EntityState.Modified;
+        //             _db.SaveChanges();
+        //             return RedirectToAction("Index");
+        //         }
 
-//         public ActionResult Delete(int id)
-//         {
-//             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-//             return View(thisCuisine);
-//         }
+        //         public ActionResult Delete(int id)
+        //         {
+        //             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+        //             return View(thisCuisine);
+        //         }
 
-//         [HttpPost, ActionName("Delete")]
-//         public ActionResult DeleteConfirmed(int id)
-//         {
-//             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-//             _db.Cuisines.Remove(thisCuisine);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
-//     }
-// }
+        //         [HttpPost, ActionName("Delete")]
+        //         public ActionResult DeleteConfirmed(int id)
+        //         {
+        //             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+        //             _db.Cuisines.Remove(thisCuisine);
+        //             _db.SaveChanges();
+        //             return RedirectToAction("Index");
+        //         }
+    }
+}
