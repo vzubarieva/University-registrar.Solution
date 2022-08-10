7619e4db05dd836317e9badccb5febe43fcee5ef
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-09 18:59:51 -0700

    add student info into departments

--- University-registrar/Controllers.cs/DepartmentsController.cs
+++ University-registrar/Controllers.cs/DepartmentsController.cs
@@ -29,7 +29,7 @@ namespace UniversityRegistrar.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Department department, int CourseId)
+        public ActionResult Create(Department department, int CourseId, int StudentId)
         {
             _db.Departments.Add(department);
             _db.SaveChanges();
@@ -44,6 +44,18 @@ namespace UniversityRegistrar.Controllers
                 );
                 _db.SaveChanges();
             }
+
+            if (StudentId != 0)
+            {
+                _db.StudentDepartments.Add(
+                    new StudentDepartment()
+                    {
+                        StudentId = StudentId,
+                        DepartmentId = department.DepartmentId
+                    }
+                );
+                _db.SaveChanges();
+            }
             return RedirectToAction("Index");
         }
 
@@ -52,6 +64,8 @@ namespace UniversityRegistrar.Controllers
             var thisDepartment = _db.Departments
                 .Include(department => department.JoinEntities)
                 .ThenInclude(join => join.Course)
+                .Include(department => department.JoinStudentDepartment)
+                .ThenInclude(join => join.Student)
                 .FirstOrDefault(department => department.DepartmentId == id);
             return View(thisDepartment);
         }
@@ -62,6 +76,8 @@ namespace UniversityRegistrar.Controllers
                 department => department.DepartmentId == id
             );
             ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+            ViewBag.StudentId = new SelectList(_db.Students, "StudentId", "Name");
+
             return View(thisDepartment);
         }
 
